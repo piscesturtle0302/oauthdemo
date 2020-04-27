7a484702e253f1505d436d4702c026f67e58a615
adanhuang0302 <adan.huang@tmnewa.com.tw>
2020-04-27 16:51:27 +0800

    new project

---
+++ src/main/java/com/example/oauthdemo/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.example.oauthdemo;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(OauthdemoApplication.class);
+	}
+
+}
